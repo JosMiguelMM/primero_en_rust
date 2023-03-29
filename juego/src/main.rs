74321818641803fe510cc9e119a1c6404ca81dbb
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2023-03-29 00:04:29 -0500

    se inicia a crear la estructura

--- juego/src/main.rs
+++ juego/src/main.rs
@@ -13,6 +13,10 @@ struct DatosHistoria {
     vida: i32,
 }
 
+//impl DatoHistoria{
+
+///}
+
 fn main() {
     let mut datos_historia: Vec<DatosHistoria>= vec![];
     let content = fs::read_to_string(FILENAME).unwrap(); //lee el archivo .csv y poder mostrar los datos a travez de la consola
@@ -20,11 +24,16 @@ fn main() {
 
     //a travez del siguiente for se muestran los datos del archivo .csv en columnas
     for result in rdr.records() {
+
+        let result = result.unwrap();
+        let vida=result.get(3).unwrap().trim();
+        let vida:i32=vida.parse::<i32>().unwrap_or(0);
+
         let dato = DatosHistoria {
-            tipo: result.as_ref().unwrap().get(0).unwrap().trim().to_string(),
-            tag: result.as_ref().unwrap().get(1).unwrap().trim().to_string() ,
-            texto: result.as_ref().unwrap().get(2).unwrap().trim().to_string(),
-            vida:0,
+            tipo: result.get(0).unwrap().trim().to_string(),
+            tag: result.get(1).unwrap().trim().to_string() ,
+            texto: result.get(2).unwrap().trim().to_string(),
+            vida:vida,
         };
         //datos_historia.push(dato);
         println!("{:?}", dato)
